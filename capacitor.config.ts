db227997651652a292aaa50b8a047a1eec546d77
veronicatia <veronicatianingrum@mail.ugm.ac.id>
2024-08-25 19:40:05 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'pgpblanjut-app',
+  webDir: 'www'
+};
+
+export default config;
